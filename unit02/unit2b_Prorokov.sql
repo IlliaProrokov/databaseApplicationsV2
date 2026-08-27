adece3abacaf8e71fe25a3564424f3bdf25f6f0c
IlliaProrokov <mc_iprorokov27@mcjvs2.org>
2026-08-27 13:39:59 -0400

    changes to file structure

--- unit02/unit2b_Prorokov.sql
+++ unit02/unit2b_Prorokov.sql
@@ -11,34 +11,48 @@
 
 
 -- 1. Show every team in Ohio or California, with its state.
-
-
--- 2. Show teams founded between 1960 and 1980, with their founding
+SELECT full_name, state 
+FROM teams
+WHERE NOT (state = 'California');
+-- 2. Show teams founded c 1960 and 1980, with their founding
 --    years. Use BETWEEN.
-
+SELECT full_name,state
+FROM teams
+WHERE NOT (year_founded BETWEEN 1960 AND 1980)
 
 -- 3. Show teams in Texas, Florida, or New York, with their states.
 --    Use IN.
-
+SELECT full_name, state 
+FROM teams
+WHERE NOT (state = 'california' OR state = 'Texas');
 
 -- 4. Show teams whose nickname starts with the letter C.
-
+SELECT full_name, state 
+FROM teams 
+WHERE nickname LIKE 'C%';
 
 -- 5. Find every player whose name contains "James".
-
+SELECT full_name, state
+FROM teams
+WHERE full_name LIKE '%James%'
 
 -- 6. List each state that has at least one team — each state only
 --    once, sorted alphabetically.
-
-
+SELECT DISTINCT state 
+FROM teams 
+ORDER BY state ASC;
 -- 7. Show every team that is NOT in California. Use NOT.
-
+-- Query 5 returns more than just LeBron. Who else shows up, and why?
+SELECT full_name
+FROM teams
+WHERE NOT (state = 'california');
 
 -- =====================================================================
 -- CHECK YOUR WORK
 -- =====================================================================
 
 -- Query 5 returns more than just LeBron. Who else shows up, and why?
+
 
 
 -- Query 6 returns fewer rows than there are teams. Why?
