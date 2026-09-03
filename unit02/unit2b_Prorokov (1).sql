6c8657d98a8f426177822b5b666abced81cf051e
IlliaProrokov <mc_iprorokov27@mcjvs2.org>
2026-09-03 14:02:32 -0400

    unit2b

--- unit02/unit2b_Prorokov (1).sql
+++ unit02/unit2b_Prorokov (1).sql
@@ -11,68 +11,52 @@
 
 
 -- 1. Show every team in Ohio or California, with its state.
-SELECT full_name, state
-FROM   teams
-WHERE  NOT (state = 'California');
+
 
 -- 2. Show teams founded between 1960 and 1980, with their founding
 --    years. Use BETWEEN.
-SELECT full_name, state 
-FROM teams 
-WHERE year_founded BETWEEN 1960 AND 1980
 
 
 -- 3. Show teams in Texas, Florida, or New York, with their states.
 --    Use IN.
-SELECT full_name,state
-FROM teams 
-WHERE NOT (state = 'California' OR state = 'Texas')
+
 
 -- 4. Show teams whose nickname starts with the letter C.
-SELECT full_name,state 
-FROM teams
-WHERE nickname LIKE 'C%'
+
+
 -- 5. Find every player whose name contains "James".
-SElECT full_name 
-FROM players
-WHERE full_name LIKE '%James%'
+
 
 -- 6. List each state that has at least one team — each state only
 --    once, sorted alphabetically.
-SELECT full_name 
-FROM teams 
-WHERE state <> 'California'
+
 
 -- 7. Show every team that is NOT in California. Use NOT.
-SELECT full_name, state
-FROM   teams
-WHERE  NOT (state = 'California');
+
 
 -- =====================================================================
 -- CHECK YOUR WORK
 -- =====================================================================
 
 -- Query 5 returns more than just LeBron. Who else shows up, and why?
---Because alot people has a name like "James" 
+
 
 -- Query 6 returns fewer rows than there are teams. Why?
---because its show just once
+
 
 -- Query 7 could also be written with <> instead of NOT. Why do they
 -- give the same result?
---It is perhaps more useful when the condition is bigger than one comparison:
-
 
 
 -- =====================================================================
 -- VOCABULARY — your words, not the reference sheet's
 -- =====================================================================
 
--- Boolean logic: analys what we has 
+-- Boolean logic:
 
 
--- Wildcard: say NOT 
+-- Wildcard:
 
 
--- NULL: change answer 
+-- NULL:
 
